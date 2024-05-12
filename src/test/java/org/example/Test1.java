7318eef55df7d4cb8cd5fe5d1581c9907e2dfcd5
BosincianuAlex <bos.roberto@ymail.com>
2024-05-12 19:57:45 +0300

    push

--- src/test/java/org/example/Test1.java
+++ src/test/java/org/example/Test1.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v85.network.Network;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -12,13 +15,15 @@ import org.testng.annotations.Test;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Test1 extends TestConfig {
 
     @Test
     public void addToWishlistTest() throws InterruptedException {
-
+        Object[][] data1 = {{1,2,3},{456}};
+        Object data2[][] ={{},{}};
         List<String> productNames = MainPage.getProductNames();
         /*
         js.executeScript("arguments[0].scrollIntoView(true);",MainPage.sizeDropDown);
@@ -50,6 +55,16 @@ public class Test1 extends TestConfig {
         return new Object[][]{{"pantofi ", "adidas", "36"},{"pantofi ", "nike", "36"}};
     }
 
+    @Test
+    public void networkTest()
+    {
+        ChromeDriver driver = new ChromeDriver();
+        DevTools devTools = driver.getDevTools();
+        devTools.send(Network.enable(Optional.empty(),Optional.empty(),Optional.empty()));
+
+
+
+    }
 }
 
 
